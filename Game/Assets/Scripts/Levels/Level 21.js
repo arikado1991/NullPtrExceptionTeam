330e9ff7c5c6044878f3dd7a52fa3c6fbc46fd5f
Eric Miller <eric@legoaces.org>
2015-03-11 19:58:27 -0400

    Added two levels, initially set to 21 and 22

---
+++ Game/Assets/Scripts/Levels/Level 21.js
@@ -0,0 +1,45 @@
+﻿function GenerateLevel(grid:Grid){
+	var block: SpaceBox;
+	var pos: Vector3;
+	
+	grid.SetGrid(7,10,7);
+	for (var x: int=0; x<7; x++){
+		for (var z: int=0;z<7;z++){
+			if (z == 2 && x > 1 && x < 5) continue;
+			grid.CreateBlock(Vector3(x,0,z));			
+		}
+	}
+	
+	for (x=0; x<7; x++){
+		//for (var z: int=0;z<7;z++){
+			if (x == 3) continue;
+			grid.CreateBlock(Vector3(x,1,3));			
+		//}
+	}
+	
+	grid.CreateBlock(Vector3(4,2,3));
+	grid.CreateBlock(Vector3(5,2,3));
+	grid.CreateBlock(Vector3(4,3,3));
+	grid.CreateBlock(Vector3(5,3,3));
+	grid.CreateBlock(Vector3(6,2,3));
+	grid.CreateBlock(Vector3(6,3,3));
+	
+	for (x=0; x<7; x++){
+		for (z=4;z<7;z++){
+//			if (x == 3) continue;
+			if (x == 1 && z == 6) continue;
+			grid.CreateBlock(Vector3(x,1,z));
+			if (x < 3) continue;
+			grid.CreateBlock(Vector3(x,2,z));
+			grid.CreateBlock(Vector3(x,3,z));
+		}
+	}
+//	
+//	grid.CreateBlock(Vector3(8,2,3));
+	grid.CreateIceBlock(Vector3(3, 1, 1));
+	grid.CreateIceBlock(Vector3(1, 2, 4));
+	var button:Block=grid.CreateButton(Vector3(1,3,6),[Vector3(2,0,2),Vector3(3,0,2),Vector3(4,0,2)],[],false);
+	
+	grid.SpawnCharacter(Vector3(1,5,1));
+	grid.CreateDestination(Vector3(3, 4, 4));
+}
---
+++ Game/Assets/Scripts/Levels/Level 22.js
@@ -0,0 +1,34 @@
+﻿function GenerateLevel(grid:Grid){
+	var block: SpaceBox;
+	var pos: Vector3;
+	
+	grid.SetGrid(7,10,7);
+	for (var x: int=0; x<7; x++){
+		for (var z: int=0;z<7;z++){
+			grid.CreateBlock(Vector3(x,0,z));
+			if (z == 0 && (x == 4 || x == 5 || x == 6)) continue;
+			grid.CreateBlock(Vector3(x,5,z));
+		}
+	}
+	
+	grid.CreateBlock(Vector3(6,1,4));
+	grid.CreateIceBlock(Vector3(4, 1, 4));
+	grid.CreateIceBlock(Vector3(5, 1, 1));
+	grid.CreateIceBlock(Vector3(2, 1, 3));
+	
+	grid.CreateBlock(Vector3(4,2,2));
+	grid.CreateBlock(Vector3(4,3,1));
+	grid.CreateBlock(Vector3(4,3,0));
+	grid.CreateBlock(Vector3(5,4,0));
+	
+	
+	grid.CreateBlock(Vector3(5,6,5));
+	grid.CreateBlock(Vector3(5,7,5));
+	//var button:Block=grid.CreateButton(Vector3(1,3,6),[Vector3(2,0,2),Vector3(3,0,2),Vector3(4,0,2)],[],false);
+//	grid.CreateIceBlock(Vector3(4, 6, 3));
+	grid.CreateIceBlock(Vector3(5, 6, 3));
+//	grid.CreateIceBlock(Vector3(2, 6, 2));
+	
+	grid.SpawnCharacter(Vector3(1,1,1));
+	grid.CreateDestination(Vector3(5,8,5));
+}
