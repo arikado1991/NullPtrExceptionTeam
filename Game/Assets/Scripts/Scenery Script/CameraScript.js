edafb19a0d08b52970cd393799b47a7bb1a8a39e
arikado1991 <phan_t1@denison.edu>
2015-03-11 15:27:01 -0400

    Fix Camera

--- Game/Assets/Scripts/Scenery Script/CameraScript.js
+++ Game/Assets/Scripts/Scenery Script/CameraScript.js
@@ -4,22 +4,49 @@ var TopView: Vector3;
 var Origin: Vector3;
 var isShaking: boolean;
 function Awake(){
-	NormalView = Vector3(-.5, 4, -5);
+	NormalView = Vector3(-.5, 4, -3);
 	TopView =  Vector3(-.5, 4, 0);
-	Origin = Vector3(-.5, 4, -5);
+	Origin = Vector3(-.5, 4, -3);
+}
+
+function rotate(deg: float){
+	for(var i: int = 0; i < 10; i++){
+				transform.RotateAround(transform.position, Vector3.left,deg/10);
+				yield WaitForSeconds(0.02);//yield WaitForSeconds(0.1);
+		}
 }
 function SwitchView( top:boolean){
+	var dir: Vector3;
+	var i: int;
+	var time: float= .3;
+	var startTime:float = Time.time;
 	if (top && Origin != TopView){
+		
+		dir = TopView  - Origin;
+	
+		StartCoroutine(rotate(-45));
+		for(i = 0; i < 10; i++){
+				transform.localPosition += dir*.1;
+				yield WaitForSeconds(0.01);
+		}
 		Origin = TopView;
-		transform.localPosition = TopView;
-		transform.RotateAround(Vector3.left, -45);
 	}
-	else if (Origin != NormalView){
+	else if (!top && Origin != NormalView){
+
+		dir= NormalView  - Origin;
+		StartCoroutine(rotate(45));
+		for(i = 0; i < 10; i++){
+				transform.localPosition += dir*.1;
+				yield WaitForSeconds(0.01);
+		}
 		Origin = NormalView;
-		transform.localPosition = NormalView;
-		transform.RotateAround(Vector3.left, 45);
 	}
+	transform.localPosition = Origin;
 }
+
+
+
+
 
 function Shake(){
 	isShaking = true;
