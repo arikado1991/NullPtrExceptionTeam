f41b3c4cee158572b44a96369488b2a67ec0e504
arikado1991 <phan_t1@denison.edu>
2015-03-11 14:10:35 -0400

    Safe2Pull

--- Game/Assets/Scripts/Character.js
+++ Game/Assets/Scripts/Character.js
@@ -36,6 +36,7 @@ class Character extends System.Object
 			return desiredPos + Vector3.up;} //Step up}
 
 		
+
 		if (!grid.hasStandable(desiredPos) && grid.hasStandable(desiredPos + Vector3.down) )
 		{	willJump = true;
 			return desiredPos + Vector3.down;} //Step down}
@@ -73,11 +74,9 @@ class Character extends System.Object
 			}
 		return desiredPos;
 	}
-	
 	function checkBelow(grid:Grid){
 		return grid.getSpaceBox(pos+Vector3.down);
 	}
-	
 	function move( dir: Dir, grid: Grid )
 	{	
 		var fig: Transform= prefab.FindGameObjectWithTag("Rufus").transform;
@@ -100,38 +99,16 @@ class Character extends System.Object
 			default:
 				finalR = 3;				
 			}
-			
 			fig.transform.RotateAround(fig.transform.position, Vector3.up,90*(this.dirction-finalR));
 			this.dirction=finalR;
 			return;
 		}
-<<<<<<< HEAD
 
 		var target:Vector3 = motionTarget(dir, grid);
-=======
-		
-		var x:Vector3=spaceInFront(pos,dir);
-		
-		/*if (grid.grid[x.x,x.y-1,x.z]==null)
-		{
-			return;
-		}
-		
-		//Debug.Log("Got here");
-		
-		below = checkBelow(grid);
-		if ( below.type == SType.BOX && (below as Block).bType == BoxType.BUTTON){
-			(below as ButtonBox).Release();
-		}*/
-		
-		var target:Vector3 = motionTarget(dir, grid);
-		isMoving = true;
->>>>>>> origin/Additional_Levels
 	
 		var startTime:float = Time.time;
 		var startPos:Vector3 = pos;
 		var endPos:Vector3 = target;
-<<<<<<< HEAD
 		if (endPos!= startPos)
 		{
 			below = checkBelow(grid);
@@ -162,59 +139,18 @@ class Character extends System.Object
 					grid.state = GridState.FINISHED;
 			}
 			below = checkBelow(grid);
+			Debug.Log(below.GetType());
 			if (below.type == SType.BOX && (below as Block).bType == BoxType.BUTTON){
 				(below as ButtonBox).getPushed();
 			}
-=======
-		if(willJump) { 
-			prefab.BroadcastMessage("Jump");
-			//Debug.Log(startPos); 
-			//Debug.LogError(endPos); 
-			yield (WaitForSeconds(.1));
-		}
-		else prefab.BroadcastMessage("Walk");
-		
-		/*while (Time.time < startTime + time){
->>>>>>> origin/Additional_Levels
 			
-			prefab.transform.position = Vector3.Slerp(startPos, endPos, (Time.time - startTime)/time);
-			yield;
-		}*/
 		
-		for (var i = 0; i < 3; i++){
-			prefab.transform.position = Vector3.Slerp(startPos, endPos, (i+1)/3);
-			yield WaitForSeconds(.02);
-			//yield;
-		}
-		
-		prefab.BroadcastMessage("Stop");
-		pos = endPos;
-		var yum: SpaceBox = grid.getSpaceBox(pos);
-		if (yum != null && yum.type == SType.EDIBLE)
-		{
-			if((yum as Edible).eType == EdibleType.DEST)
-				grid.state = GridState.FINISHED;
-		}
-		
-		
-//		Debug.Log(endPos.ToString());
-		var oldBelow=grid.getSpaceBox(startPos+Vector3.down);
-		if (startPos!=endPos && oldBelow.type == SType.BOX && (oldBelow as Block).bType == BoxType.BUTTON)
-		{
-			(oldBelow as ButtonBox).Release();
-		}
-		
-
-		below = checkBelow(grid);
-		
-		if (below.type == SType.BOX && (below as Block).bType == BoxType.BUTTON){
-			(below as ButtonBox).getPushed();
 			prefab.FindGameObjectWithTag("MainCamera").BroadcastMessage("SwitchView",grid.hasBox(pos+Vector3.back));
 
 		}
 		isMoving = false;
 		willJump = false;
-		//Debug.LogError(prefab.transform.position); 
+		
 		return;
 	}
 
