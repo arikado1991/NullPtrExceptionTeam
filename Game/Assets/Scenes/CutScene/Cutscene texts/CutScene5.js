172564732f4567db4dd78d29881f8f001e3b81f0
arikado1991 <phan_t1@denison.edu>
2015-03-12 03:52:11 -0400

    Add cutscenes

---
+++ Game/Assets/Scenes/CutScene/Cutscene texts/CutScene5.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var bg:Texture;
+function Awake(){
+		
+		this.GetComponent(GUITexture).texture = bg;
+		this.GetComponent(RollingScript).texts = [
+		"Oh my god Rufus, are you okay.",
+		"Hey calm down.",
+		"Could you just be quiet!",
+		"You think you are the only one who feel this way",
+		"What don't I know?",
+		"The feeling when they ignore you?",
+		"The feeling when they pass by as if you're no one?",
+		"The same and the same, everyday?",
+		"Doesn't the name 'Spike' ring a bell to you?", ""
+		];
+
+}
+function LoadScene()
+{
+	Application.LoadLevel("Level 5");
+
+}
---
+++ Game/Assets/Scenes/CutScene/Cutscene texts/CutScene7.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var bg:Texture;
+function Awake(){
+		
+	this.GetComponent(GUITexture).texture = bg;
+	this.GetComponent(RollingScript).texts = [
+	"<sign> I guess it really doesn't matter...",
+	"Well it's been too long to matter anyway.",
+	"You want to know how did I ?",
+	"I don't really want to relive that, not that I really can ...",
+	"Let's focus on getting you out of here first.",
+	"Your time is almost up.",
+	"You need to hurry before your real body ceases to function properly",
+	"Follow me! I know a shortcut...",
+	"... but we need to go through my consicousness ...", ""
+	];
+
+}
+function LoadScene()
+{
+	Application.LoadLevel("Level 7");
+
+}
