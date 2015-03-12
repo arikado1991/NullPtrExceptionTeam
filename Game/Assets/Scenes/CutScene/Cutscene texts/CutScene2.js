6b299c64d34e2a6c9634e829fa6aa7dc5d0fd388
arikado1991 <phan_t1@denison.edu>
2015-03-12 01:37:10 -0400

    z

--- Game/Assets/Scenes/CutScene/Cutscene texts/CutScene2.js
+++ Game/Assets/Scenes/CutScene/Cutscene texts/CutScene2.js
@@ -15,25 +15,21 @@ function Awake(){
 		"This is nothing compared to what I've eaten!",
 		"With this you should be able to wake up and get help.",
 		"You don't have much time, just press the emergency button",
-		"<GASP>", "<PAIN>, <PAIN>", "<SIREN SOUND>", "Nurse: My goodness, we need someone here quickly!",
+		"<gasp>", "<pain>, <pain>", "<siren sound>", "Nurse: My goodness, we need someone here quickly!",
 		"...",
 		"...",
 		"Doctor: That was close.",
-<<<<<<< Updated upstream
-		"..."
-		
-=======
 		"...",
-		"Oh you came to...",
+		"Oh you came to... Welcome back",
 		"You're in your dream again of course.",
 		"I guess it makes sense since you're gonna stick around for a while...",
-		"You can call me Spike!"
->>>>>>> Stashed changes
-		];
+		"You can call me Spike!",
+		"<woman screaming>",
+		"Wendy who's that??",
+		"Rufus! Wait!",""
 		
-//		counter = 0;
-//	BroadcastMessage("setBackground", bg);
-//	BroadcastMessage("setTexts", texts);
+		];
+
 }
 function LoadScene()
 {
