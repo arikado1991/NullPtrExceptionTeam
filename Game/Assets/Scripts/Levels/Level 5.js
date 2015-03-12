15af7448200baee71253261647406e64dcb9e73d
Eric Miller <eric@legoaces.org>
2015-03-12 00:34:20 -0400

    Finished prettying up Level 5

--- Game/Assets/Scripts/Levels/Level 5.js
+++ Game/Assets/Scripts/Levels/Level 5.js
@@ -28,9 +28,25 @@
 	var button:Block=grid.CreateButton(Vector3(13,1,5),[Vector3(11,2,7),Vector3(11,3,8),Vector3(11,4,9)],[],false);
 	
 	grid.SpawnCharacter(Vector3(1,5,1));
-	grid.CreateDestination(Vector3(1, 5, 13));
+	grid.CreateDestination(Vector3(10, 5, 7));
 	
 	grid.CreatePizza(Vector3(6,4,3));
 	grid.CreatePizza(Vector3(0,5,6));
 	grid.CreatePizza(Vector3(11,4,8));
+}
+
+var instruct: int=0;
+function OnGUI(){
+
+	var buttonW:int = 400;
+	var buttonH:int = 100;
+	if (instruct==0){
+	 GUI.Window (0, Rect(Screen.width/2-buttonW/2, 10, buttonW, buttonH), InstructionFunction, 
+	 	"You're on your own now.\n(Press space to dismiss)");
+	}
+}
+
+function InstructionFunction(windowID: int){
+	if (Input.GetKeyUp("space"))
+		instruct++;
 }
