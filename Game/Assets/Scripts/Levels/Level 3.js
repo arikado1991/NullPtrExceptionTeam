e7ffb0b876859de7fd4a4daf8bed6f8398fd69bd
Eric Miller <eric@legoaces.org>
2015-03-12 00:08:37 -0400

    Through level 3 is now pretty

--- Game/Assets/Scripts/Levels/Level 3.js
+++ Game/Assets/Scripts/Levels/Level 3.js
@@ -3,58 +3,28 @@
 function GenerateLevel(grid:Grid){
 	var block: SpaceBox;
 	var pos: Vector3;
-	grid.SetGrid(11,10,10);
-	for (var i: int=0; i<11; i++){
-		for (var j: int=0;j<10;j++){
-			grid.CreateBlock(Vector3(i,0,j));
-			if (i==0)
-			{
-				grid.CreateBlock(Vector3(i,1,j));
-				grid.CreateBlock(Vector3(i,2,j));
-				grid.CreateBlock(Vector3(i,3,j));
-			}
-			if (i==10)
-			{
-				grid.CreateBlock(Vector3(i,1,j));
-				grid.CreateBlock(Vector3(i,2,j));
-				grid.CreateBlock(Vector3(i,3,j));
-			}
-			if (j>=5)
-			{
-				grid.CreateBlock(Vector3(i,1,j));
-				grid.CreateBlock(Vector3(i,2,j));
-			}
-			
-		}
-	}
+	grid.SetGrid(5,10,7);
 	
-	grid.SpawnCharacter(Vector3(1,9,1));
-	grid.CreateIceBlock(Vector3(3,1,2));
-	grid.CreateDestination(Vector3(5, 3, 8));
+	grid.BuildRect(Vector3(0, 0, 0), Vector3(4,0,6),"box");
+	grid.BuildRect(Vector3(0, 0, 4), Vector3(4,2,6),"box");
+
+	grid.SpawnCharacter(Vector3(1,1,1));
+	grid.CreateIceBlock(Vector3(3,1,1));
+	grid.CreateDestination(Vector3(2, 3, 4));
 }
 
-
-function setDestination(grid:Grid, location:Vector3){
-	grid.getSpaceBox(location).destination=true;
-	grid.getSpaceBox(location).prefab.transform.GetChild(7).renderer.material=materialDest;
-}
-
-var windowRect : Rect = Rect (Screen.width/2-150, 10, 300, 200);
-var buttonW:int = 200;
-var buttonH:int = 100;
 var instruct: int=0;
 function OnGUI(){
+
+	var buttonW:int = 400;
+	var buttonH:int = 100;
 	if (instruct==0){
-	 GUI.Window (0, Rect(Screen.width/2-150, 10, 300, 70), InstructionFunction, "Press Z to push the green cube\n (Press space to continue)");
-	 }
-	if (instruct==1){
-	 GUI.Window (0, Rect(Screen.width/2-150, 10, 300, 70), InstructionFunction, "Step on it to climb the cliff\n (Press space to continue)");
+	 GUI.Window (0, Rect(Screen.width/2-buttonW/2, 10, buttonW, buttonH), InstructionFunction, 
+	 	"That cliff is awfully tall...\nPress the 'Z' key while facing the green block\nReset button in top right\n(Press space to dismiss)");
 	}
 }
 
 function InstructionFunction(windowID: int){
 	if (Input.GetKeyUp("space"))
-	{
-		instruct+=1;
-	}
+		instruct++;
 }
