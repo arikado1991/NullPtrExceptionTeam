077cd29d07a68f378298990b9f2087665f2db9e2
Eric Miller <eric@legoaces.org>
2015-03-11 22:51:21 -0400

    Renamed all levels to match logical order

--- Game/Assets/Scripts/Levels/Level 4.js
+++ Game/Assets/Scripts/Levels/Level 4.js
@@ -1,32 +1,43 @@
-﻿function GenerateLevel(grid:Grid){
+﻿var materialDest:   Material;
+
+function GenerateLevel(grid:Grid){
 	var block: SpaceBox;
 	var pos: Vector3;
 	
-	grid.SetGrid(15,10,15);
-	for (var i: int=0; i<15; i++){
-		for (var j: int=0;j<15;j++){
-			if (j<6 && (i==5 || i==6 || i==7 ||i==8))
-			{
-				if(j==3 && i!=8){
-				grid.CreateBlock(Vector3(i,3,j));}
-				continue;
-			}
-			grid.CreateBlock(Vector3(i,0,j));
-			grid.CreateBlock(Vector3(i,1,j));
-			
-			if (j>=6 && i<=10)
-			{
-				grid.CreateBlock(Vector3(i,2,j));
-				grid.CreateBlock(Vector3(i,3,j));
-				grid.CreateBlock(Vector3(i,4,j));
-			}
-			
-		}
-	}
-	grid.CreateBlock(Vector3(8,2,3));
-	grid.CreateIceBlock(Vector3(1, 2, 3));
-	var button:Block=grid.CreateButton(Vector3(13,1,1),[Vector3(11,2,7),Vector3(11,3,8),Vector3(11,4,9)],[],false);
+	grid.SetGrid(10,10,10);
 	
-	grid.SpawnCharacter(Vector3(1,5,1));
-	grid.CreateDestination(Vector3(1, 5, 13));
+	grid.BuildRect(Vector3(0, 0, 0), Vector3(9,0,5),"box");
+	grid.BuildRect(Vector3(0, 1, 4), Vector3(4,2,4), "box");
+	grid.CreateIceBlock(Vector3(1,1,3));
+	grid.CreateButton(Vector3(4, 1, 3), [Vector3(5,1,6),Vector3(5,1,5),Vector3(5,1,7)],[Vector3(0, 1, 4)], false);
+	grid.BuildRect(Vector3(0, 0, 7), Vector3(9,0,9),"box");
+	
+	grid.SpawnCharacter(Vector3(0,9,0));
+	grid.CreateDestination(Vector3(9,1,9));
+}
+
+
+function setDestination(grid:Grid, location:Vector3){
+	grid.getSpaceBox(location).destination=true;
+	grid.getSpaceBox(location).prefab.transform.GetChild(7).renderer.material=materialDest;
+}
+
+var windowRect : Rect = Rect (Screen.width/2-150, 10, 300, 200);
+var buttonW:int = 200;
+var buttonH:int = 100;
+var instruct: int=0;
+function OnGUI(){
+	if (instruct==0){
+	 GUI.Window (0, Rect(Screen.width/2-150, 10, 300, 70), InstructionFunction, "Use arrow keys to move\n (Press space to continue)");
+	 }
+	if (instruct==1){
+	 GUI.Window (0, Rect(Screen.width/2-150, 10, 300, 70), InstructionFunction, "Move to the red cube\n (Press space to continue)");
+	}
+}
+
+function InstructionFunction(windowID: int){
+	if (Input.GetKeyUp("space"))
+	{
+		instruct+=1;
+	}
 }
---
+++ Game/Assets/Scripts/Levels/Level 6.js
@@ -0,0 +1,58 @@
+﻿function GenerateLevel(grid:Grid){
+	var block: SpaceBox;
+	var pos: Vector3;
+	
+	grid.SetGrid(10,11,10);
+	for (var i: int=0; i<10; i++){
+		for (var j: int=0;j<10;j++){
+			
+			
+			grid.CreateBlock(Vector3(i,0,j));
+			grid.CreateBlock(Vector3(i,1,j));
+			grid.CreateBlock(Vector3(i,2,j));
+			grid.CreateBlock(Vector3(i,3,j));
+		}
+	}
+	
+	grid.BuildRect(Vector3(0, 4, 4), Vector3(5,6,9),"box");
+	grid.BuildRect(Vector3(0, 6, 7), Vector3(2,8,9),"box");
+	
+	
+	grid.BuildRect(Vector3(0, 4, 0), Vector3(5,4,3),"box");
+	grid.BuildRect(Vector3(0, 5, 0), Vector3(3,5,3),"box");
+	grid.BuildRect(Vector3(0, 6, 0), Vector3(1,6,3),"box");
+	grid.CreateBlock(Vector3(0,9,9));
+	
+	grid.CreateIceBlock(Vector3(8, 4, 1));
+	grid.CreateIceBlock(Vector3(8, 5, 1));
+	grid.CreateIceBlock(Vector3(8, 6, 1));
+	grid.CreateIceBlock(Vector3(8, 7, 1));
+	
+	
+	var Block1=[Vector3(6,4,4), Vector3(7,4,4),Vector3(8,4,4),Vector3(9,4,4),Vector3(6,5,4), Vector3(7,5,4),Vector3(8,5,4),Vector3(9,5,4),Vector3(6,6,4), Vector3(7,6,4),Vector3(8,6,4),Vector3(9,6,4)];
+	var Block2=[Vector3(6,4,6), Vector3(7,4,6),Vector3(8,4,6),Vector3(9,4,6),Vector3(6,5,6), Vector3(7,5,6),Vector3(8,5,6),Vector3(9,5,6),Vector3(6,6,6), Vector3(7,6,6),Vector3(8,6,6),Vector3(9,6,6)];
+	var Block3=[Vector3(6,4,8), Vector3(7,4,8),Vector3(8,4,8),Vector3(9,4,8),Vector3(6,5,8), Vector3(7,5,8),Vector3(8,5,8),Vector3(9,5,8),Vector3(6,6,8), Vector3(7,6,8),Vector3(8,6,8),Vector3(9,6,8)];
+	for (i=0;i<12;i++)
+	{
+		grid.CreateBlock(Block1[i]);
+		grid.CreateBlock(Block2[i]);
+		grid.CreateBlock(Block3[i]);
+	}
+	
+	var b1=grid.CreateButton(Vector3(5,6,4),Block1,[],false);
+	b1.reverse=true;
+	b1.mode=1;
+	var b2=grid.CreateButton(Vector3(5,6,6),Block2,[],false);
+	b2.reverse=true;
+	b2.mode=1;
+	var b3=grid.CreateButton(Vector3(5,6,8),Block3,[],false);
+	b3.reverse=true;
+	b3.mode=1;
+	
+	grid.CreatePizza(Vector3(3,6,2));
+	grid.CreatePizza(Vector3(9,8,5));
+	grid.CreatePizza(Vector3(9,7,9));
+	
+	grid.SpawnCharacter(Vector3(9,5,0));
+	grid.CreateDestination(Vector3(0, 10, 9));
+}
